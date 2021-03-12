fa23b6cce3c83779758f312369f70ddc0dcb6d15
stmechanic <kovalev.ilya@mail.ru>
2021-03-12 11:47:52 +0300

    1. Создать модель User [#415944]

--- src/main/java/map/User.java
+++ src/main/java/map/User.java
@@ -3,7 +3,13 @@ package map;
 import java.util.Calendar;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
 }
